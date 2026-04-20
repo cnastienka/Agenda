4d63ce59979fc5c4c718ff953134decc92c4db78
DaniHdzS <he317832@uaeh.edu.mx>
2026-04-20 14:34:31 -0600

    Corrección de sintaxis en código

--- src/com/agenda/services/AgendaService.java
+++ src/com/agenda/services/AgendaService.java
@@ -1,31 +1,32 @@
 package com.agenda.services;
 
 public class AgendaService {
-}
 
-public void añadirContacto(Contacto c) {
+
+    public void añadirContacto(Contacto c) {
 //Indica si hay espacio de almacenamiento máximo 10 contactos.
-    if (contador >= 10) {
-        System.out.println("Agenda llena(máximo 10 contactos)");
-        return;
-    }
-//Valida que no este vacio nombre y apellido
-    if (c.getNombre() == null || c.getNombre().isEmpty() ||
-            c.getApellido() == null || c.getApellido().isEmpty()) {
-        System.out.println("El nombre y apellido no pueden estar vacíos.");
-        return;
-    }
-//Comprobar que no exista ya el contacto (nombre y apellido iguales=duplicado)
-    for (int i = 0; i < contador; i++) {
-        if (contactos[i].getNombre().equalsIgnoreCase(c.getNombre()) &&
-                contactos[i].getApellido().equalsIgnoreCase(c.getApellido())) {
-            System.out.println("El contacto ya existe.");
+        if (contador >= 10) {
+            System.out.println("Agenda llena(máximo 10 contactos)");
             return;
         }
-    }
-    //Añadir el contacto
-    contactos[contador] = c;
-    contador++;
+//Valida que no este vacio nombre y apellido
+        if (c.getNombre() == null || c.getNombre().isEmpty() ||
+                c.getApellido() == null || c.getApellido().isEmpty()) {
+            System.out.println("El nombre y apellido no pueden estar vacíos.");
+            return;
+        }
+//Comprobar que no exista ya el contacto (nombre y apellido iguales=duplicado)
+        for (int i = 0; i < contador; i++) {
+            if (contactos[i].getNombre().equalsIgnoreCase(c.getNombre()) &&
+                    contactos[i].getApellido().equalsIgnoreCase(c.getApellido())) {
+                System.out.println("El contacto ya existe.");
+                return;
+            }
+        }
+        //Añadir el contacto
+        contactos[contador] = c;
+        contador++;
 
-    System.out.println("Contacto añadido correctamente.");
+        System.out.println("Contacto añadido correctamente.");
+    }
 }
