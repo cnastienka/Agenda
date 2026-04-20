4e9ed9c27faf7d3923bfef3de27cf02a4b63b608
DaniHdzS <he317832@uaeh.edu.mx>
2026-04-20 13:58:08 -0600

    Método añadirContacto

---
+++ .idea/src/services/AgendaService.java
@@ -0,0 +1,28 @@
+package Agenda;
+
+public void añadirContacto(Contacto c) {
+//Indica si hay espacio de almacenamiento máximo 10 contactos.
+    if (contador >= 10) {
+        System.out.println("Agenda llena(máximo 10 contactos)");
+        return;
+    }
+//Valida que no este vacio nombre y apellido
+    if (c.getNombre() == null || c.getNombre().isEmpty() ||
+            c.getApellido() == null || c.getApellido().isEmpty()) {
+        System.out.println("El nombre y apellido no pueden estar vacíos.");
+        return;
+    }
+//Comprobar que no exista ya el contacto (nombre y apellido iguales=duplicado)
+    for (int i = 0; i < contador; i++) {
+        if (contactos[i].getNombre().equalsIgnoreCase(c.getNombre()) &&
+                contactos[i].getApellido().equalsIgnoreCase(c.getApellido())) {
+            System.out.println("El contacto ya existe.");
+            return;
+        }
+    }
+    //Añadir el contacto
+    contactos[contador] = c;
+    contador++;
+
+    System.out.println("Contacto añadido correctamente.");
+}
